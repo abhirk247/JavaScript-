e2d74b8a954b9db1ce6fde050cb2039b06841e5c
Abhishek Anand <abhirk247@gmail.com>
2022-06-27 19:15:12 +0530

    finsih variables and constants

--- script.js
+++ script.js
@@ -1,3 +1,55 @@
+/*  ----let --------
+
 let total=149.99;
 
 let product='Hiking Boots';
+
+let discounted=true;
+
+let welcome='Welcome';
+
+let prie=45.55,
+    name='Hiking',
+    discount=true;
+
+*/
+
+
+    // ------valid variable names-----
+
+/*
+    a
+    account
+    account_99
+    accountNumber
+    _accountNumber
+    $accountNumber
+    _123
+    _proto_
+    
+    */
+
+    // let 99times=99;  // it give error
+
+    // let price=99.34;
+
+    // price=45.34;
+
+    // ----constant ----  
+/*
+    const price; // it give error
+    const price=40;
+
+    price=34; // it give error
+ 
+    */
+
+    // ------var keyword------
+/*
+    console.log(price);
+    var price=25; 
+*/
+
+
+
+
